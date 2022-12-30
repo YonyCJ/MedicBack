aba6ce462323704e082eb1ef341165f373c1a87a
YonyCoyla <jonyd718@gmail.com>
2022-12-30 14:48:37 -0500

    Clase 2

--- src/main/java/com/example/medic/model/Persona.java
+++ src/main/java/com/example/medic/model/Persona.java
@@ -1,6 +1,12 @@
 package com.example.medic.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "persona")
 public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nombres;
 }
